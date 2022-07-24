ae03dfa82be7acd3241fe44aed51b2d442b3b6b8
Tyler Adam Martinez <tylermartinez@my.unt.edu>
2022-07-24 15:09:02 -0500

    [BookCatalog] feat: add default image to Book Component

--- library-system/src/components/Book.js
+++ library-system/src/components/Book.js
@@ -1,10 +1,17 @@
-
+import {useRef} from 'react';
+import DefaultImage from '../../src/images/book-icon-sign-design-260nw-553945819.webp';
 
 function Book (props) {
+    const imgRef = useRef();
+    function onImageError() {
+        imgRef.current.src = DefaultImage;
+    }
 
     return (
         <div className="thumbnail">
-            <img 
+            <img
+                ref={imgRef}
+                onError={onImageError}
                 src={props.Image} 
                 alt={`book of ${props.Title}`}
                 style={{width: 140, height: "auto"}}
--- library-system/src/pages/bookcatalog.js
+++ library-system/src/pages/bookcatalog.js
@@ -1,80 +1,11 @@
-
+import Book from "../components/Book";
 
 function bookcatalog() {
     return (
       <div className="rowcontainer">
-        <div className="thumbnail" title="honeyandspice">
-          <a href="honeyandspice.html">
-            <img
-              src="images/HoneyandSpice.jpg"
-              alt="Honey & Spice Book Cover"
-              style={{width: 140, height: "auto"}}
-              title="honeyandspice"
-            />
-          </a>
-        <p>Honey & Spice</p>
-      </div>
-
-      <div className="thumbnail" title="thelioness">
-        <a href="thelioness.html">
-          <img
-            src="images/TheLioness.jpg"
-            alt="The Lioness Book Cover"
-            style={{width: 140, height: "auto"}}
-            title="thelioness"
-          />
-        </a>
-        <p>The Lioness</p>
-      </div>
-
-      <div className="thumbnail" title="horse">
-        <a href="horse.html">
-          <img
-            src="images/Horse.jpg"
-            alt="Horse Book Cover"
-            style={{width: 140, height: "auto"}}
-            title="horse"
-          />
-        </a>
-        <p>Horse</p>
-      </div>
-
-      <div className="thumbnail" title="anotherfamily">
-        <a href="anyotherfamily.html">
-          <img
-            src="images/AnyOtherFamily.jpg"
-            alt="Horse Book Cover"
-            style={{width: 140, height: "auto"}}
-            title="anotherfamily"
-          />
-        </a>
-        <p>Any Other Family</p>
-      </div>
-
-      <div className="thumbnail" title="counterfeit">
-        <a href="counterfeit.html">
-          <img
-            src="images/Counterfeit.jpg"
-            alt="Counterfeit Book Cover"
-            sstyle={{width: 140, height: "auto"}}
-            title="counterfeit"
-          />
-        </a>
-        <p>Counterfeit</p>
-      </div>
-
-      <div className="thumbnail" title="cultclassNameic">
-        <a href="cultclassNameic.html">
-          <img
-            src="images/CultclassNameic.jpg"
-            alt="CultclassNameic Book Cover"
-            style={{width: 140, height: "auto"}}
-            title="cultclassNameic"
-          />
-        </a>
-        <p>Cult classNameic</p>
-      </div>
-    </div> );
+        <Book Title="title1" Image="NotBase64string" />        
+      </div> 
+    );
 }
 
 export default bookcatalog;
