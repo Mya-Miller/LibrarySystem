adb23f8cd8c1414f5b3aa10c6deef97186597e9f
Tyler Adam Martinez <tylermartinez@my.unt.edu>
2022-07-24 13:15:32 -0500

    [NavBar] mv navbar logic to react

--- library-system/src/App.js
+++ library-system/src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
+import UNTLogo from '../src/images/UNTLogo.png';
 
 function App() {
+  const [IsAuth, SetIsAuth] = useState(false);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div class="header">
+        <div class="logo_btn">
+          <div class="untlogo">
+            <img
+              src={UNTLogo}
+              alt="UNT Logo"
+              style={{"width": 100, "height": 100}}
+            />
+          </div>
+          <h1>University of North Texas eLibrary</h1>
+        </div>
+      </div>
+      <div class="navbar">
+        <div class="logintoggle">
+          {
+            IsAuth ? 
+            <div class="login">
+              <a href="login.html" class="login">Login</a>
+            </div>
+            :
+            <div class="logout">
+              <input id="btnLogout" type="button" value="Logout" />
+            </div>
+          }
+        </div>
+        <a href="index.html">View eBook Catalog</a>
+        <a href="reservestudyroom.html">Reserve Study Room</a>
+        <a href="checkout.html">Checkout</a>
+        <div>
+          <button class="btn">Dark Mode</button>
+        </div>
+      </div>
     </div>
   );
 }
