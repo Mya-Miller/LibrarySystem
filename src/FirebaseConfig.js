4b2e65c226f1935ea74a473d2efc78d701595254
Mya-Miller <101530032+Mya-Miller@users.noreply.github.com>
2022-07-27 13:18:51 -0500

    Added popups for each book

--- src/FirebaseConfig.js
+++ src/FirebaseConfig.js
@@ -2,6 +2,7 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getAnalytics } from 'firebase/analytics';
 
+
 const config = {
     apiKey: process.env.API_KEY,
     authDomain: process.env.AUTH_DOMAIN,
--- src/components/Book.js
+++ src/components/Book.js
@@ -1,12 +1,11 @@
 import {useRef} from 'react';
 import DefaultImage from '../../src/images/book-icon-sign-design-260nw-553945819.webp';
 import React from 'react';
-import {useState} from 'react';
+import Popup from 'reactjs-popup';
 import './Book.css';
 
 function Book (props) {
     const imgRef = useRef();
-    const [buttonPopup, setButtonPopup] = useState(false);
     
     function onImageError() {
         imgRef.current.src = DefaultImage;
@@ -14,7 +13,6 @@ function Book (props) {
 
     return (
         <div className="thumbnail">
-            <button onClick={() => setButtonPopup(true)}>
             <img
                 ref={imgRef}
                 onError={onImageError}
@@ -22,31 +20,32 @@ function Book (props) {
                 alt={`book of ${props.Title}`}
                 style={{width: 140, height: "auto"}}
             />
-            <p>{props.Title}</p>
-            </button>
-            <Popup tigger={buttonPopup} setTrigger ={setButtonPopup}>
-            <p>
-                {props.Title}
-                {props.Description}
-                {props.id}
-                {props.Author}
-                {props.Genre}
-            </p>
+            <Popup trigger={
+                <button>
+                    {props.Title}
+                </button> 
+            } position="right center">
+                <p>
+                    {props.Title}<br></br>
+                    {props.Description}<br></br>
+                    {props.id}<br></br>
+                    {props.Author}<br></br>
+                    {props.Genre}<br></br>
+                </p>
             </Popup>
         </div>
-
     );
 }
 
-function Popup(props){
-    return( props.trigger) ? (
-        <div className='popup'> 
-        <div className='popup-inner'>
-            <button className='close-btn' onClick={() => props.setTrigger(false)}>close</button>
-            {props.children}
-        </div>
-        </div>
-    ) : "";
-}
+// function Popup(props){
+//     return( props.trigger) ? (
+//         <div className='popup'> 
+//         <div className='popup-inner'>
+//             <button className='close-btn' onClick={() => props.setTrigger(false)}>close</button>
+//             {props.children}
+//         </div>
+//         </div>
+//     ) : "";
+// }
 
 export default Book;
