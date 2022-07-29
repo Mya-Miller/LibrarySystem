55f31a51d802800901a64dc416d38e8c98809c4d
Mya-Miller <101530032+Mya-Miller@users.noreply.github.com>
2022-07-29 15:43:06 -0500

    Popups (#19)
    * Login (#16) (#17)
    
    * [Login] Updated login form CSS
    
    * [Login] Update login and registration forms
    
    * Update FirebaseConfig.js
    
    Co-authored-by: Emmie <36717379+Emmie8@users.noreply.github.com>
    
    * added popup logic
    
    * added all info for popups
    
    * added missing variable
    
    * Added pop up styling
    
    * imported book.css
    
    * Added popups for each book
    
    Co-authored-by: Emmie <36717379+Emmie8@users.noreply.github.com>

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,8 +1,12 @@
 import {useRef} from 'react';
 import DefaultImage from '../../src/images/book-icon-sign-design-260nw-553945819.webp';
+import React from 'react';
+import Popup from 'reactjs-popup';
+import './Book.css';
 
 function Book (props) {
     const imgRef = useRef();
+    
     function onImageError() {
         imgRef.current.src = DefaultImage;
     }
@@ -16,10 +20,32 @@ function Book (props) {
                 alt={`book of ${props.Title}`}
                 style={{width: 140, height: "auto"}}
             />
-            <p>{props.Title}</p>
+            <Popup trigger={
+                <button>
+                    {props.Title}
+                </button> 
+            } position="right center">
+                <p>
+                    {props.Title}<br></br>
+                    {props.Description}<br></br>
+                    {props.id}<br></br>
+                    {props.Author}<br></br>
+                    {props.Genre}<br></br>
+                </p>
+            </Popup>
         </div>
-
     );
 }
+
+// function Popup(props){
+//     return( props.trigger) ? (
+//         <div className='popup'> 
+//         <div className='popup-inner'>
+//             <button className='close-btn' onClick={() => props.setTrigger(false)}>close</button>
+//             {props.children}
+//         </div>
+//         </div>
+//     ) : "";
+// }
 
 export default Book;
