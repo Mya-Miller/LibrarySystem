4100c3572531e710bf5b45bac965a4b4cf2e3d7b
Mya-Miller <101530032+Mya-Miller@users.noreply.github.com>
2022-08-01 12:46:06 -0500

    [Catalog] Add Sort (#24) (#25)
    * [Catalog] Set up sort features
    
    * [Catalog] Add sort functionality
    
    * [Catalog] Update sort buttons styling
    
    * [Catalog] Add sort option
    
    Co-authored-by: Emmie <36717379+Emmie8@users.noreply.github.com>

--- src/pages/bookcatalog.js
+++ src/pages/bookcatalog.js
@@ -14,23 +14,72 @@ function Bookcatalog() {
     const booksData = await BookCrudServices.getAllBooks();
     setBooks(booksData.docs.map((doc) => ({...doc.data(), id: doc.id})))
   }
+
+  function sortAscending() {
+    const ascending = [...books].sort((a,b) => {
+      return a.Title > b.Title ? 1 : -1
+    })
+    setBooks(ascending)
+  }
+
+  function sortDescending() {
+    const descending = [...books].sort((a,b) => {
+      return a.Title < b.Title ? 1 : -1
+    })
+    setBooks(descending)
+  }
+
+  function sortAuthor() {
+    const author = [...books].sort((a,b) => {
+      return a.Author > b.Author ? 1 : -1
+    })
+    setBooks(author)
+  }
+
+  function sortGenre() {
+    const author = [...books].sort((a,b) => {
+      return a.Genre > b.Genre ? 1 : -1
+    })
+    setBooks(author)
+  }
+
+  function sortISBN() {
+    const isbn = [...books].sort((a,b) => {
+      return a.id > b.id ? 1 : -1
+    })
+    setBooks(isbn)
+  }
   
   return (
-    <div id="bookCatalogRow">
-      {
-        books.map((book, index) => {
-          return (
-            <Book
-              key={book.id}
-              Title={book.Title}
-              Image={book.Image}
-              Description={book.Description}
-              Author={book.Author}
-              Genre={book.Genre}
-            />
-          );
-        })
-      }
+    <div className="catalog">
+      <div className="sortbtncontainer">
+      {/*<div className="dropdown">
+      <button className="dropdownBtn">Sort</button>
+      <div className="sortoptions">*/}
+      <button onClick={() => { sortAscending() }} className="sortBtn Ascending">Ascending</button>
+      <button onClick={() => { sortDescending() }} className="sortBtn Descending">Descending</button>
+      <button onClick={() => { sortAuthor() }} className="sortBtn Author">Author</button>
+      <button onClick={() => { sortGenre() }} className="sortBtn Genre">Genre</button>
+      <button onClick={() => { sortISBN() }} className="sortBtn ISBN">ISBN</button>
+      </div>
+      {/*</div>
+      </div>*/}
+      <div id="bookCatalogRow">
+        {
+          books.map((book, index) => {
+            return (
+              <Book
+                key={book.id}
+                Title={book.Title}
+                Image={book.Image}
+                Description={book.Description}
+                Author={book.Author}
+                Genre={book.Genre}
+              />
+            );
+          })
+        }
+      </div>
     </div>
   )
 }
