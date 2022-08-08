ca60d73c11ae89b767ea25782b115c80fb3888d8
Emmie <36717379+Emmie8@users.noreply.github.com>
2022-08-08 01:05:01 -0500

    [App] Removed unused code and updated styling

--- src/services/Checkout.services.js
+++ src/services/Checkout.services.js
@@ -1,39 +1,21 @@
 import {db} from '../FirebaseConfig';
-import { getAuth } from 'firebase/auth';
 import { 
     collection, 
     getDocs, 
     setDoc  
 } from 'firebase/firestore';
 
-function getCollectionPath() {
-    const auth = getAuth();
-    const user = auth.currentUser;
-    
-    if (user !== null) {  
-        console.log("Sign-in provider: " + user.providerId);
-        console.log("  Provider-specific UID: " + user.uid);
-        console.log("  Name: " + user.displayName);
-        console.log("  Email: " + user.email);
-        console.log("  Photo URL: " + user.photoURL); 
-        const id = user.uid;
-        const collection = "CheckoutLogs/" + id + "/BookList";
-        return id;
-    }
-    else {
-        return "CheckoutLogs"
-    } 
-}
-
-const collectionPath = getCollectionPath();
-const CheckoutCollectionRef = collection(db, collectionPath);
+const uid = sessionStorage.getItem('userUID');
+const bookCollectionPath = "CheckoutLogs/" + uid + "/BookList";
+const bookListCollectionRef = collection(db, bookCollectionPath);
+const CheckoutCollectionRef = collection(db, "CheckoutLogs");
 
 class CheckoutDataService {
     addUserLog = (newUserLog) => { 
         return setDoc(CheckoutCollectionRef, newUserLog);
     }
-    getCartBooks = () => {
-        return getDocs(CheckoutCollectionRef);
+    getCheckoutBooks = () => {
+        return getDocs(bookListCollectionRef);
     }
 }
 
